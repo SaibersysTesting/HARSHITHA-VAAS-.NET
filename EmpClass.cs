fd26ed773271e793b02cf97da57f85d1b109db63
arun teja <training.saibersys9@gmail.com>
2016-06-17 14:39:34 -0400

    Add files via upload

---
+++ EmpClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsEx
+{
+    class EmpClass
+    {
+        public int empno;
+        public string empname;
+        public double sal;
+
+        public void getDetails(int no,string name,double salary)
+        {
+            empno = no;
+            empname = name;
+            sal = salary;
+        }
+        public void disply()
+        {
+        Console.WriteLine("Employee ID number:"+empno +"\nEmployee name:"+empname +"\nsalary:"+sal);
+        }
+    }
+}
---
+++ mainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsEx
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            EmpClass EC = new EmpClass();
+            EC.getDetails(100, "harshitha", 8000);
+            EC.disply();
+
+            EC.getDetails(200, "vaas", 9000);
+            EC.disply();
+
+            
+        }
+    }
+}
